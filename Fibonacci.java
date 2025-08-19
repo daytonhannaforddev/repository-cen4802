0f9ee632bbf88b861014cd64e56e13408beed9d5
Dayton Hannaford <144299432+daytonhannaforddev@users.noreply.github.com>
2025-08-19 19:23:14 -0400

    Assignment 1: Fibonacci

---
+++ Fibonacci.java
@@ -0,0 +1,13 @@
+public class Fibonacci {
+
+    public static int fibonacci(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+    public static void main(String[] args) {
+        int n = 10;
+        int y = fibonacci(n);
+        System.out.println("The " + n + "th term of the Fibonacci sequence is " + y + ".");
+    }
+}
